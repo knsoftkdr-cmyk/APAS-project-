d596d15d33c54185beacd74dc29ae52d581bd1fb
knsoftkdr-cmyk <knsoft.kdr@gmail.com>
2026-06-05 18:21:40 +0530

    ui/ux changes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -301,35 +301,35 @@ const TeacherHome = () => {
             backgroundPosition: "center",
           }}
         >
-          <div
+<div
   className="absolute inset-0 bg-gradient-to-r
-  from-[#EDE9FE]/100
-  via-[#DDD6FE]/90
-  to-[#C4B5FD]/85"
+  from-[#2563EB]/80
+  to-[#60A5FA]/100"
 />
 <div className="absolute top-6 right-40 w-14 h-14 rounded-full border border-white/40"></div>
           <div className="absolute bottom-10 right-80 w-8 h-8 rounded-full border border-white/40"></div>
-          <div className="absolute top-16 left-1/2 w-6 h-6 rounded-full border border-white/80"></div>
+          <div className="absolute top-16 left-1/2 w-6 h-6 rounded-full border border-white/50"></div>
 
-                    <div className="absolute top-12 left-[45%] text-white/80 text-xl">✦</div>
+                    <div className="absolute top-12 left-[45%] text-white/40 text-xl">✦</div>
           <div className="absolute bottom-16 left-[60%] text-white/50 text-lg">✦</div>
-          <div className="absolute top-24 right-[35%] text-white/80 text-lg">✦</div>
+          <div className="absolute top-24 right-[35%] text-white/40 text-lg">✦</div>
           
-          <div className="absolute top-6 left-1/4 text-white/50 text-xl">✦</div>
+          <div className="absolute top-6 left-1/4 text-white/40 text-xl">✦</div>
           <div className="absolute top-0 left-[45%] text-white/40 text-lg">✦</div>
           <div className="absolute top-1/2 left-[70%] text-white/40 text-lg">✦</div>
-          <div className="absolute top-24 right-[45%] text-white/90 text-lg">✦</div>
+          <div className="absolute top-24 right-[45%] text-white/40 text-lg">✦</div>
 
           <div className="absolute top-12 right-64 w-0 h-0 border-l-[12px] border-l-transparent border-r-[12px] border-r-transparent border-b-[20px] border-b-white/40"></div>
 
           <div className="absolute bottom-16 left-72 w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-b-[18px] border-b-white/40"></div>
 
-          <div className="absolute top-28 left-1/3 w-0 h-0 border-l-[8px] border-l-transparent border-r-[8px] border-r-transparent border-b-[14px] border-b-white/80"></div>
+          <div className="absolute top-28 left-1/3 w-0 h-0 border-l-[8px] border-l-transparent border-r-[8px] border-r-transparent border-b-[14px] border-b-white/40"></div>
 
              <img
     src={teacherhomebanner}
     alt="Teacher Home Dashboard"
-    className="absolute right-8 -bottom-14 h-[405px] object-contain"
+    /* className="absolute right-8 -bottom-14 h-[405px] object-contain" */
+    className="hidden md:block absolute right-0 bottom-6 w-[450px]"
   />
           <div className="relative z-10 max-w-2xl">
             <div className="flex items-center gap-2 mb-3">
